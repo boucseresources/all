cc3ab833acfc877b2da5be496490493690ec3b53
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2025-07-20 20:36:47 +0600

    google ashbe eibar

---
+++ docs/meta-inject.js
@@ -0,0 +1,8 @@
+// meta-inject.js
+function metaloadHoyeja() {
+  const meta = document.createElement("meta");
+  meta.name = "google-site-verification";
+  meta.content = "2DzSK5YqtUmF6BuC8wVWanf-j8tYFIEKnuQPi6Vkq0o";
+  document.getElementsByTagName("head")[0].appendChild(meta);
+}
+metaloadHoyeja();
